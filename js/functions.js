6f185a12ddd25e4c208608b6ae4fa3161f9ae018
AhmaduLo <gayeahmadubamba@gmail.com>
2021-03-03 19:53:24 +0100

    first commit

---
+++ js/functions.js
@@ -0,0 +1,41 @@
+function sound() {
+  window.addEventListener("keydown", function (e) {
+    let key = e.keyCode;
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+
+    const btnContainer = audio.nextElementSibling;
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    btnContainer.classList.add("sound-active");
+
+    audio.addEventListener("ended", function () {
+      btnContainer.classList.remove("sound-active");
+    });
+  });
+}
+
+function soundMobile() {
+  window.addEventListener("keydown", function (e) {
+    let target = e.target;
+
+    let key = target.getAttribute("id");
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    target.classList.add("sound-active");
+
+    audio.addEventListener("ended", function () {
+      target.classList.remove("sound-active");
+    });
+  });
+}
+
+export function all() {
+  sound();
+  soundMobile();
+}
